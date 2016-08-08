0c5e944b882dbeaf703348c79f7cdd1522512cc4
Ricky <Ricky@DESKTOP-PH4SIEC>
2016-08-08 22:47:47 +0300

    Add project files.

---
+++ EF.Test/GeoAsset.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Test
+{
+    public class GeoDbContext : DbContext
+    {
+        public GeoDbContext()
+            : base() {
+
+        }
+
+        public DbSet<GeoAsset> GeoAssets { get; set; }
+        public DbSet<GeoAssetRelationship> GeoAssetRelationships { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public enum GeoAssetType
+    {
+        Port = 1,
+        PortGroup = 2
+    }
+
+    public enum GeoAssetRelationshipType
+    {
+        PortGroup = 1
+    }
+
+    [Table("GeoAsset",Schema ="Geo")]
+    public class GeoAsset
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GeoAssetId { get; set; }
+        public string GeoAssetName { get; set; }
+        public GeoAssetType GeoAssetType { get; set; }
+
+        [ForeignKey("GeoAssetIdFrom")]
+        public ICollection<GeoAssetRelationship> GeoAssetRelationshipsFrom { get; set; }
+
+        [ForeignKey("GeoAssetIdTo")]
+        public ICollection<GeoAssetRelationship> GeoAssetRelationshipTo { get; set; }
+
+    }
+
+    [Table("GeoAssetRelationship", Schema = "Geo")]
+    public class GeoAssetRelationship
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GeoAssetRelationshipId { get; set; }
+        public int? GeoAssetIdFrom { get; set; }
+        public int? GeoAssetIdTo { get; set; }
+
+        public GeoAssetRelationshipType GeoAssetRelationshipType { get; set; }
+
+        [ForeignKey("GeoAssetIdFrom")]
+        public GeoAsset GeoAssetFrom { get; set; }
+
+        [ForeignKey("GeoAssetIdTo")]
+        public GeoAsset GeoAssetTo { get; set; }
+    }
+}
---
+++ EF.Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Test
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var context = new GeoDbContext();
+
+            //var geoAssetFirst = new GeoAsset() {
+            //    GeoAssetName = "Geo1",
+            //    GeoAssetType = GeoAssetType.Port
+            //};
+
+            //var geoAssetSecond = new GeoAsset() {
+            //    GeoAssetName = "PortGroupTest",
+            //    GeoAssetType = GeoAssetType.PortGroup
+            //};
+
+            //var geoAssetThird = new GeoAsset() {
+            //    GeoAssetName = "Geo2",
+            //    GeoAssetType = GeoAssetType.Port
+            //};
+
+            //var geoAssetFirst = context.GeoAssets.Where(x => x.GeoAssetName == "Geo1").FirstOrDefault();
+            //var geoPortGroup = context.GeoAssets.Where(x => x.GeoAssetType == GeoAssetType.PortGroup).FirstOrDefault();
+
+            //context.GeoAssets.Add(geoAssetThird);
+
+            //var geoRelationship = new GeoAssetRelationship() {
+            //    GeoAssetFrom = geoAssetThird,
+            //    GeoAssetTo = geoPortGroup,
+            //    GeoAssetRelationshipType = GeoAssetRelationshipType.PortGroup
+            //};
+
+            //context.GeoAssetRelationships.Add(geoRelationship);
+
+            //context.SaveChanges();
+
+            //select all ports of the Parent Port
+            var geoGroup = context.GeoAssetRelationships.Where(x => x.GeoAssetIdTo == 2).Select(x => x.GeoAssetFrom).ToList();
+
+            var geoFromGroup = context.GeoAssets.Where(x => x.GeoAssetId == 2)
+                                                .Include(x=>x.GeoAssetRelationshipsFrom)
+                                                .Include(x=>x.GeoAssetRelationshipTo)
+                                                .FirstOrDefault();
+        }
+    }
+}
